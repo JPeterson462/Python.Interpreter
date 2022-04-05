3b17e5717a4cb19dbf5c7e502011d50d648360a1
Jonathan Peterson <jpeterson462@gmail.com>
2022-04-05 19:41:35 -0400

    JP: Updated TODO

--- Python.Interpreter/Program.cs
+++ Python.Interpreter/Program.cs
@@ -91,10 +91,9 @@ namespace Python.Interpreter
 
             //PythonParser p = ParsingUnitTest("f\"abc{de}\"");
             //PythonParser p = ParsingUnitTest("def greeting(name: str) -> str:\n\treturn 'Hello ' + name");
-            PythonParser p = ParsingUnitTest("def broadcast_message(message: str, servers: Sequence[tuple[tuple[str, int], dict[str, str]]]) -> None:\n\tpass");
+            //PythonParser p = ParsingUnitTest("def broadcast_message(message: str, servers: Sequence[tuple[tuple[str, int], dict[str, str]]]) -> None:\n\tpass");
 
-
-            //PythonParser p = ParsingUnitTest("def broadcast_message(\n   message: str,\n   servers: Sequence[tuple[tuple[str, int], dict[str, str]]])->None:\n\tpass");
+            PythonParser p = ParsingUnitTest("def broadcast_message(\n   message: str,\n   servers: Sequence[tuple[tuple[str, int], dict[str, str]]])->None:\n\tpass");
 
             PrintTokens(p.Tokens);
 
